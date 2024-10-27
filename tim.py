a8279f530c18e584de6f49c76a01f174db9b82b7
life-david <dodat2795@gmail.com>
2024-10-27 23:25:29 +0700

    update all

--- tim.py
+++ tim.py
@@ -1,3 +1,4 @@
+
 import random
 from math import sin, cos, pi, log
 from tkinter import *
@@ -7,7 +8,7 @@ CANVAS_HEIGHT = 480
 CANVAS_CENTER_X = CANVAS_WIDTH / 2 
 CANVAS_CENTER_Y = CANVAS_HEIGHT / 2 
 IMAGE_ENLARGE = 11 
-HEART_COLOR = "#8A2BE2" 
+HEART_COLOR = "#8A2BE2"
 
 
 def heart_function(t, shrink_ratio: float = IMAGE_ENLARGE):
